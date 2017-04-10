82b318c6688091f7c68c50f287184c5f43689b83
Matheus Magalhães <matheusr@cpqd.com.br>
2017-04-10 11:19:08 -0300

    [IOTMID-155] Small fixes for page headers
    This commit fixes some positioning and styling issues
    related to the page header area.
    
    Change-Id: Ie5417b55455d61d945553f4ebdfae958ae0432f9

--- src/js/containers/full/PageHeader.js
+++ src/js/containers/full/PageHeader.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 function PageHeader(props) {
   return (
     <div className="page-header">
-      <div className="title-area">
-        <div className="page-title caps">{props.title}</div>
-        <div className="page-subtitle">{props.subtitle}</div>
-      </div>
-      <div className="page-actions">
-        {props.children}
+      <div className="data">
+        <div className="title-area">
+          <div className="page-title caps">{props.title}</div>
+          <div className="page-subtitle">{props.subtitle}</div>
+        </div>
+        <div className="page-actions">
+          {props.children}
+        </div>
       </div>
     </div>
   )

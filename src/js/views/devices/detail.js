a6fe78378d9abbc49007ebc681b9337cbe15e308
Hermes Vieira Junior <hjunior@cpqd.com.br>
2017-05-26 15:02:44 -0300

    [IOTMID-388] Device details page
    - This commit changes the device details page to show:
      - The attributes data correctly
      - The status of device
      - The position data of geo attributes.
    
    Change-Id: Id186b6d1acf6233cbebbda8f5861a801ea59f766

--- src/js/views/devices/detail.js
+++ src/js/views/devices/detail.js
@@ -7,10 +7,15 @@ import { Link, hashHistory } from 'react-router'
 
 import alt from '../../alt';
 import AltContainer from 'alt-container';
+import MeasureStore from '../../stores/MeasureStore';
+import MeasureActions from '../../actions/MeasureActions';
 import DeviceActions from '../../actions/DeviceActions';
 import deviceManager from '../../comms/devices/DeviceManager';
 import DeviceStore from '../../stores/DeviceStore';
 import util from "../../comms/util/util";
+
+import { Line } from 'react-chartjs-2';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 class FActions {
   set(args) { return args; }
@@ -94,37 +99,311 @@ class CreateDeviceActions extends Component {
   }
 }
 
-class AttrCard extends Component {
+function TagList (props) {
+  const tags = props.tags;
+  return (
+    <span>
+      { (tags.length > 0) ? (
+        tags.map((tag) =>
+          <span className="tag" key={tag}>
+            <i className="fa fa-tag"></i>{tag}
+          </span>
+        )
+      ) : (
+        <sapn className="tag">No tags set</sapn>
+      )}
+    </span>
+  )
+}
+
+class Graph extends Component{
   constructor(props) {
     super(props);
   }
 
   render() {
-    const hasValue = (this.props.value && this.props.value.length > 0);
-    const splitSize = "col " + (hasValue ? " s6" : " s12");
+    let labels = [];
+    let values = [];
+    this.props.data.map((i) => {
+      if (i.attrValue.trim().length > 0) {
+        labels.push(util.printTime(Date.parse(i.recvTime)/1000));
+        if (i.attrType.toLowerCase() === 'integer') {
+          values.push(parseInt(i.attrValue));
+        } else if (i.attrType.toLowerCase() === 'float') {
+          values.push(parseFloat(i.attrValue));
+        } else {
+          console.error('unknown field type');
+          values.push(parseInt(i.attrValue));
+        }
+      }
+    })
+
+    if (values.length == 0) {
+      return (
+        <div className="valign-wrapper full-height background-info">
+          <div className="full-width center">No data available</div>
+        </div>
+      )
+    }
+
+    let filteredLabels = labels.map((i,k) => {
+      if ((k == 0) || (k == values.length - 1)) {
+        return i;
+      } else {
+        return "";
+      }
+    })
+
+    const data = {
+      labels: labels,
+      xLabels: filteredLabels,
+      datasets: [
+        {
+          label: 'Device data',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: values
+        }
+      ]
+    }
+
+    const options = {
+      maintainAspectRatio: false,
+      legend: { display: false },
+      scales: {
+        xAxes: [
+          { display: false },
+          { ticks: { autoSkip: true, maxRotation: 0, minRotation: 0 }}
+        ],
+      }
+    }
 
     return (
-      <div className="col s12 m6 l4">
-        <div className="card z-depth-2">
-          <div className="card-content row">
-            <div className="col s12 main">
-              <div className="value title">{this.props.name}</div>
-              <div className="label">Name</div>
-            </div>
-            <div className={splitSize}>
-              <div className="value">{this.props.type}</div>
-              <div className="label">Type</div>
-            </div>
-            {(hasValue > 0) && (
-              <div className={splitSize}>
-                <div className="value">{this.props.value}</div>
-                <div className="label">Value</div>
-              </div>
-            )}
+      <Line data={data} options={options}/>
+    )
+  }
+}
+class PositionRenderer extends Component {
+  componentDidMount(){}
+
+  render() {
+    function NoData() {
+      return (
+        <div className="full-height valign-wrapper background-info subtle relative graph">
+          <div className="horizontal-center">
+            <i className="material-icons">report_problem</i>
+            <div>No position data available</div>
           </div>
         </div>
+      )
+    }
+
+    if ((this.props.attr == null) ||
+        (! (this.props.deviceId in this.props.devices)) ||
+        (! (this.props.attr in this.props.devices[this.props.deviceId]))) {
+      return (<NoData />);
+    }
+
+    let pos = this.props.value;
+    let parsed = null;
+    if (pos === undefined) {
+      const posData = this.props.devices[this.props.deviceId][this.props.attr];
+      if (!posData.loading) {
+        pos = posData.data[posData.data.length - 1].attrValue;
+      } else {
+        return (
+          <div className="background-info valign-wrapper full-height relative bg-gray">
+            <i className="fa fa-circle-o-notch fa-spin fa-fw horizontal-center"/>
+          </div>
+        )
+      }
+    }
+
+    parsed = pos.match(/^([+-]?\d+(\.\d+)?)\s*[,]\s*([+-]?\d+(\.\d+)?)$/)
+    if (parsed == null) {
+      return (<NoData />)
+    }
+
+    const position = [parseFloat(parsed[1]),parseFloat(parsed[3])];
+    return (
+      <Map center={position} zoom={19}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}></Marker>
+      </Map>
+    )
+  }
+}
+
+class Position extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if ((this.props.position != null) && ('name' in this.props.position)) {
+      MeasureActions.fetchMeasures.defer(this.props.device.id, this.props.device.protocol, this.props.position);
+    }
+  }
+
+  render () {
+    let geoAttr = null;
+    let value = undefined;
+    if ((this.props.position != null) && ('name' in this.props.position)) {
+      geoAttr = this.props.position.name;
+      value = this.props.position.value;
+    }
+
+    return (
+      <AltContainer store={MeasureStore}>
+        <PositionRenderer deviceId={this.props.device.id} attr={geoAttr} value={value}/>
+      </AltContainer>
+    )
+  }
+}
+
+function HistoryList(props) {
+  let trimmedList = props.data.filter((i) => {
+    return i.attrValue.trim().length > 0
+  })
+  trimmedList.reverse();
+
+  if (trimmedList.length > 0) {
+    return (
+      <div className="full-height scrollable history-list">
+        {trimmedList.map((i,k) =>
+          <div className={"row " + (k % 2 ? "alt-row" : "")} key={i.recvTime}>
+            <div className="col s12 value">{i.attrValue}</div>
+            <div className="col s12 label">{util.printTime(Date.parse(i.recvTime)/1000)}</div>
+          </div>
+        )}
       </div>
     )
+  } else {
+    return (
+      <div className="full-height background-info valign-wrapper center">
+        <div className="center full-width">No data available</div>
+      </div>
+    )
+  }
+}
+
+function Attr(props) {
+  const known = {
+    'integer': Graph,
+    'float': Graph,
+    'string': HistoryList,
+    'default': HistoryList
+    // TODO to be implemented
+    // 'geo': PositionHistory,
+  }
+
+  const Renderer = props.type in known ? known[props.type] : known['default'];
+  return (
+    <Renderer {...props} />
+  )
+}
+
+class DetailAttrs extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.device.attrs.map((i) => {
+      MeasureActions.fetchMeasures.defer(this.props.device.id, this.props.device.protocol, i);
+    })
+  }
+
+  render() {
+    const device = this.props.device;
+
+    let filteredStatics = this.props.device.static_attrs.filter((a) => { return (a.type.toLowerCase() != "geo")});
+
+    function AttrList(props) {
+      return (
+        <span>
+          { device.attrs.map((i, k) =>
+              <div className={"col s6 metric-card full-height mb20"} key={i.object_id} >
+                {(props.devices[device.id] && props.devices[device.id][i.name] &&
+                  (props.devices[device.id][i.name].loading == false)) ? (
+                  <div className="graphLarge z-depth-2 full-height">
+                    <div className="title row">
+                      <span>{i.name}</span>
+                      <span className="right"
+                            onClick={() => MeasureActions.fetchMeasures(device.id, device.protocol, i)}>
+                        <i className="fa fa-refresh" />
+                      </span>
+                    </div>
+                    <div className="contents">
+                      <Attr type={props.devices[device.id][i.name].type} data={props.devices[device.id][i.name].data}/>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="graphLarge z-depth-2 full-height">
+                    <span className="title">{i.name}</span>
+                    <div className="contents">
+                      <div className="background-info valign-wrapper full-height relative bg-gray">
+                        <i className="fa fa-circle-o-notch fa-spin fa-fw horizontal-center"/>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+          )}
+        </span>
+      )
+    }
+
+    if (filteredStatics.length > 0) {
+      count--;
+      return (
+        <div className="row half-height">
+          <div className="col s4 full-height">
+            <div className="text-info full-height">
+              <div className="title">Attributes</div>
+              <div className="">
+                <ul>
+                  {filteredStatics.map((i, k) =>
+                    (i.type.toLowerCase() != "geo") && (
+                      <li key={i.name}>
+                        <span className="col s6 label">{i.name}</span>
+                        <span className="col s6 value">{i.value}</span>
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <AttrList devices={this.props.devices} />
+        </div>
+      )
+    } else {
+      return (
+        <div className="row half-height">
+          <AttrList devices={this.props.devices} />
+        </div>
+      )
+    }
   }
 }
 
@@ -148,61 +427,96 @@ function TagList (props) {
 class DeviceForm extends Component {
   constructor(props) {
     super(props);
+    this.remove = this.remove.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('device: ', this.props.device);
+    this.props.device.attrs.map((i) => {
+      MeasureActions.fetchMeasures.defer(this.props.device.id, this.props.device.protocol, i);
+    })
+  }
+
+  remove(e) {
+    e.preventDefault();
+    DeviceActions.triggerRemoval(this.props.device, () => {
+      Materialize.toast('Device removed', 4000);
+    });
+    this.props.handleDismiss();
   }
 
   render() {
-    return (
-      <div className={"row device" + " " + (this.props.className ? this.props.className : "")}>
-        <div className="row detail-header">
-          <div className="col s12 m10 offset-m1 valign-wrapper">
-            <div className="col s3">
-              {/* TODO clickable, file upload */}
-              <div className="img">
-                <img src="images/ciShadow.svg" />
-              </div>
-            </div>
-            <div className="col s9 pt20px">
-              <div>
-                <div className="col s12">
-                  <div className="title">Name</div>
-                    <div className="user-label">{this.props.device.label}</div>
-                  </div>
-                </div>
-                <div className="s12">
-                  <div className="col s4" >
-                    <div className="title">Protocol</div>
-                    <span className="value">{this.props.device.protocol ? this.props.device.protocol : "MQTT"}</span>
-                  </div>
-                  <div className="col s8" >
-                    <div className="title">Tags</div>
-                    <TagList tags={this.props.device.tags} />
-                  </div>
-                </div>
-            </div>
-          </div>
-      </div>
+    let status = "disabled";
 
-        <div className="row">
-          <div className="col s10 offset-s1">
-            <div className="title col s12">Attributes</div>
+    if (this.props.device.status) {
+      status = this.props.device.status;
+    }
+
+    let position = null;
+    function getPosition(i) {
+      if (i.type == "geo") {
+        position = i;
+      }
+    }
+    this.props.device.static_attrs.map((i) => {getPosition(i)})
+    if (position === null) {
+      this.props.device.attrs.map((i) => {getPosition(i)})
+    }
+
+    return (
+      <div className={"lst-entry-wrapper col s12 auto-height " + status}>
+        <div className="row detail-header">
+          <div className="title">
+            <div className="label">{this.props.device.label}</div>
+            <div className="id">ID {this.props.device.id}</div>
           </div>
+          <div className="status">{status}</div>
         </div>
-        <div className="list row">
-          <div className="col s10 offset-s1">
-            { ((this.props.device.attrs.length > 0) || (this.props.device.static_attrs.length > 0) ) ? (
-              <span>
-                {this.props.device.attrs.map((attr) =>
-                  <AttrCard key={attr.object_id} {...attr}/>
-                )}
-                {this.props.device.static_attrs.map((attr) =>
-                  <AttrCard key={attr.object_id} {...attr}/>
-                )}
-              </span>
-            ) : (
-              <div className="padding10 background-info">
-                No attributes set
+        <div className="row detail-body ">
+          <div className="row content">
+            <div className="col s9 full-height">
+              <div className="row half-height">
+                <div className="col s3 full-height">
+                  <div className="img full-height">
+                    <img src="images/ciShadow.svg" />
+                  </div>
+                </div>
+                <div className="col s9">
+                  <div className="metrics col s12">
+                    <div className="metric col s4">
+                      <span className="label">Attributes</span>
+                      <span className="value">{this.props.device.attrs.length + this.props.device.static_attrs.length}</span>
+                      {/* <span className="value">{this.props.device.attrs.length}</span> */}
+                    </div>
+                    <div className="metric col s4">
+                      <span className="label">Last update</span>
+                      <span className="value">{util.printTime(this.props.device.updated)}</span>
+                    </div>
+                    <div className="metric col s4">
+                      <span className="label">Uptime</span>
+                      <span className="value">N/A</span>
+                    </div>
+                  </div>
+
+                  <div className="metrics col s12">
+                    <div className="metric col s4" >
+                      <span className="label">Protocol</span>
+                      <span className="value">{this.props.device.protocol ? this.props.device.protocol : "MQTT"}</span>
+                    </div>
+                    <div className="metric col s8" >
+                      <span className="label">Tags</span>
+                      <TagList tags={this.props.device.tags} />
+                    </div>
+                  </div>
+                </div>
               </div>
-            )}
+              <AltContainer store={MeasureStore} inject={{device: this.props.device}} >
+                <DetailAttrs />
+              </AltContainer>
+            </div>
+            <div className="col s3 map z-depth-2 full-height">
+              <Position device={this.props.device} position={position}/>
+            </div>
           </div>
         </div>
       </div>

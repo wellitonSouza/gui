8597c38de6cc7c38eb496755a583a4508b033ce7
giampaolli <giampaoli2011@gmail.com>
2018-09-12 14:26:26 -0300

    fix problem when creating a new flow

--- src/js/config.js
+++ src/js/config.js
@@ -1,199 +1,197 @@
-import L from 'leaflet'
+import L from 'leaflet';
 
-export const mapPinWhite= new L.Icon({
+export const mapPinWhite = new L.Icon({
     iconUrl: 'images/pins/white-pin.png',
     iconRetinaUrl: 'images/pins/white-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinBlack= new L.Icon({
+export const mapPinBlack = new L.Icon({
     iconUrl: 'images/pins/black-pin.png',
     iconRetinaUrl: 'images/pins/black-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinBlue= new L.Icon({
+export const mapPinBlue = new L.Icon({
     iconUrl: 'images/pins/blue-pin.png',
     iconRetinaUrl: 'images/pins/blue-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
-
-
-export const mapPinRed= new L.Icon({
+export const mapPinRed = new L.Icon({
     iconUrl: 'images/pins/red-pin.png',
     iconRetinaUrl: 'images/pins/red-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinYellow= new L.Icon({
+export const mapPinYellow = new L.Icon({
     iconUrl: 'images/pins/yellow-pin.png',
     iconRetinaUrl: 'images/pins/yellow-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinOrange= new L.Icon({
+export const mapPinOrange = new L.Icon({
     iconUrl: 'images/pins/orange-pin.png',
     iconRetinaUrl: 'images/pins/orange-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinPink= new L.Icon({
+export const mapPinPink = new L.Icon({
     iconUrl: 'images/pins/pink-pin.png',
     iconRetinaUrl: 'images/pins/pink-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinPurple= new L.Icon({
+export const mapPinPurple = new L.Icon({
     iconUrl: 'images/pins/purple-pin.png',
     iconRetinaUrl: 'images/pins/purple-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinDarkGreen= new L.Icon({
+export const mapPinDarkGreen = new L.Icon({
     iconUrl: 'images/pins/dark-green-pin.png',
     iconRetinaUrl: 'images/pins/dark-green-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinDarkYellow= new L.Icon({
+export const mapPinDarkYellow = new L.Icon({
     iconUrl: 'images/pins/dark-yellow-pin.png',
     iconRetinaUrl: 'images/pins/dark-yellow-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinBurgundy= new L.Icon({
+export const mapPinBurgundy = new L.Icon({
     iconUrl: 'images/pins/burgundy-pin.png',
     iconRetinaUrl: 'images/pins/burgundy-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinGreen= new L.Icon({
+export const mapPinGreen = new L.Icon({
     iconUrl: 'images/pins/green-pin.png',
     iconRetinaUrl: 'images/pins/green-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinLightGreen= new L.Icon({
+export const mapPinLightGreen = new L.Icon({
     iconUrl: 'images/pins/light-green-pin.png',
     iconRetinaUrl: 'images/pins/light-green-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinBeige= new L.Icon({
+export const mapPinBeige = new L.Icon({
     iconUrl: 'images/pins/beige-pin.png',
     iconRetinaUrl: 'images/pins/beige-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinDarkBlue= new L.Icon({
+export const mapPinDarkBlue = new L.Icon({
     iconUrl: 'images/pins/dark-blue-pin.png',
     iconRetinaUrl: 'images/pins/dark-blue-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinLightBlue= new L.Icon({
+export const mapPinLightBlue = new L.Icon({
     iconUrl: 'images/pins/light-blue-pin.png',
     iconRetinaUrl: 'images/pins/light-blue-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinLime= new L.Icon({
+export const mapPinLime = new L.Icon({
     iconUrl: 'images/pins/lime-pin.png',
     iconRetinaUrl: 'images/pins/lime-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });
 
 
-export const mapPinLightYellow= new L.Icon({
+export const mapPinLightYellow = new L.Icon({
     iconUrl: 'images/pins/light-yellow-pin.png',
     iconRetinaUrl: 'images/pins/light-yellow-pin.png',
     shadowUrl: 'images/pins/pin-shadow.png',
     iconAnchor: new L.Point(12, 41),
     popupAnchor: new L.Point(1, -34),
     iconSize: new L.Point(25, 41),
-    shadowSize: new L.Point(41, 41)
+    shadowSize: new L.Point(41, 41),
 });

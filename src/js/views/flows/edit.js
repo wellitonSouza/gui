00245e8ec8b46e16984d822270c2bf8e2d0c5509
Felipe Justino <felipe.jus1@gmail.com>
2018-11-05 15:52:40 -0200

    0.2Resolution800x600

--- src/js/views/flows/edit.js
+++ src/js/views/flows/edit.js
@@ -297,7 +297,7 @@ export class EditFlow extends Component {
         return (
             <ReactCSSTransitionGroup transitionName="first" transitionAppear transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={500}>
                 <NewPageHeader title="flow manager" subtitle="Flow configuration" icon="flow">
-                    <div className="row valign-wrapper full-width no-margin top-minus-2 absolute-input">
+                    <div className="row valign-wrapper absolute-input full-width no-margin top-minus-2 ">
                         <AltContainer store={FlowStore}>
                             <NameForm />
                         </AltContainer>

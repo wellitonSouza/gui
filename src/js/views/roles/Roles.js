f6bff79bb0813259a25469322dc79fc02651186f
Mariane Previde <mprevide@cpqd.com.br>
2018-11-07 17:18:45 -0200

    SideBar

--- src/js/views/roles/Roles.js
+++ src/js/views/roles/Roles.js
@@ -4,12 +4,12 @@ import TextTruncate from 'react-text-truncate';
 import { translate, Trans } from 'react-i18next';
 import RoleStore from '../../stores/RoleStore';
 import RoleActions from '../../actions/RoleActions';
+import SideBar from './SideBar';
 import { NewPageHeader } from '../../containers/full/PageHeader';
 import { DojotBtnLink } from '../../components/DojotButton';
 
 
 function RoleCard(obj) {
-    console.log(translate);
     return (
         <div className="card-size card-hover lst-entry-wrapper z-depth-2 fullHeight">
             <div className="lst-entry-title col s12 ">
@@ -44,9 +44,30 @@ function RoleCard(obj) {
 }
 
 function RoleList(param) {
+    console.log(param);
     if (param.groups) {
+        const visible = true;
+        const buttonsFooter = [
+            {
+                label: 'save',
+                alt: 'clickToSave',
+                click: param.hadleSave,
+                color: 'red',
+                modalConfirm: true,
+                modalConfirmText: 'Ctz???',
+            },
+            {
+                label: 'discard',
+                alt: 'clickToSave',
+                click: param.hadleSave,
+                color: 'red',
+                modalConfirm: true,
+                modalConfirmText: 'Ctz???',
+            },
+        ];
         return (
             <div className="fill">
+                <SideBar title={<Trans i18nKey="roles.change.title.new" />} visible={visible} buttonsFooter={buttonsFooter} />
                 {param.groups.map(obj => <RoleCard group={obj} key={obj.id} />)}
             </div>);
     }
@@ -59,7 +80,7 @@ function OperationsHeader(param) {
             <DojotBtnLink
                 responsive="true"
                 onClick={param.newGroup}
-                label="New Role"
+                label={<Trans i18nKey="roles.btn.new.text" />}
                 alt="Create a new role"
                 icon="fa fa-plus"
                 className="w130px"
@@ -85,12 +106,30 @@ class Roles extends Component {
     }
 
     newGroup() {
-        //TODO
+        console.log('newGroup');
     }
+
+    /*     newUser() {
+            const tmp = this.state;
+            tmp.createUser = true;
+            this.setState(tmp);
+            this.visibility(true, 'new');
+        }
+    
+        visibility(bool, operation) {
+            const tmp = this.state;
+            if (operation !== 'new') tmp.createUser = false;
+            tmp.visible = bool;
+            this.setState(tmp);
+        } */
 
     componentDidCatch(error, info) {
         console.log('componentDidCatch 1', error);
         console.log('componentDidCatch 2', info);
+    }
+
+    save() {
+        console.log('Oi Mundo');
     }
 
     render() {
@@ -100,7 +139,7 @@ class Roles extends Component {
                     <NewPageHeader title={<Trans i18nKey="roles.title" />} icon="roles">
                         <OperationsHeader newGroup={this.newGroup} />
                     </NewPageHeader>
-                    <RoleList />
+                    <RoleList hadleSave={this.save} />
                 </AltContainer>
             </div>
         );

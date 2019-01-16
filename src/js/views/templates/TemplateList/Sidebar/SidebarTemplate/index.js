eb60f91029f3ff54d32a787a779757a9516a63e1
giampaolli <giampaoli2011@gmail.com>
2019-01-16 11:16:45 -0200

    fix lint error

--- src/js/views/templates/TemplateList/Sidebar/SidebarTemplate/index.js
+++ src/js/views/templates/TemplateList/Sidebar/SidebarTemplate/index.js
@@ -44,7 +44,8 @@ const SidebarTemplate = ({
                                 )
                             }
                         </div>
-                    </div>)
+                    </div>
+                )
                 : <div />
             }
         </Slide>

731f8a8090af2e1fe7522a3f4d6b5517e090a888
giampaolli <giampaoli2011@gmail.com>
2019-01-29 13:45:10 -0200

    fix device create: screen does not show change in attribute

--- src/js/views/devices/Sidebar/index.js
+++ src/js/views/devices/Sidebar/index.js
@@ -149,6 +149,22 @@ class Sidebar extends Component {
                 return item;
             });
 
+            device.configValues = [];
+            device.dynamicValues = [];
+            device.staticValues = [];
+            device.actuatorValues = [];
+            device.metadata = {};
+
+            device.configValues = device.configValues.concat(device.attrs.filter(item => item.type === 'meta'));
+            device.dynamicValues = device.dynamicValues.concat(device.attrs.filter(item => item.type === 'dynamic'));
+            device.staticValues = device.staticValues.concat(device.attrs.filter(item => item.type === 'static'));
+            device.actuatorValues = device.actuatorValues.concat(device.attrs.filter(item => item.type === 'actuator'));
+            device.attrs.forEach((item) => {
+                if (Object.prototype.hasOwnProperty.call(item, 'metadata')) {
+                    device.metadata[item.id] = [...item.metadata];
+                }
+            });
+
             this.setState({
                 showDeviceAttrs: false,
                 device,
@@ -257,6 +273,28 @@ class Sidebar extends Component {
         delete formatDevice.metadata;
 
         return formatDevice;
+    }
+
+    formatDeviceAttr(device) {
+        const newDevice = { ...device };
+        console.log(newDevice)
+        newDevice.configValues = [];
+        newDevice.dynamicValues = [];
+        newDevice.staticValues = [];
+        newDevice.actuatorValues = [];
+        newDevice.metadata = {};
+
+        newDevice.configValues = device.configValues.concat(device.attrs.filter(item => item.type === 'meta'));
+        newDevice.dynamicValues = device.dynamicValues.concat(device.attrs.filter(item => item.type === 'dynamic'));
+        newDevice.staticValues = device.staticValues.concat(device.attrs.filter(item => item.type === 'static'));
+        newDevice.actuatorValues = device.actuatorValues.concat(device.attrs.filter(item => item.type === 'actuator'));
+        newDevice.attrs.forEach((item) => {
+            if (Object.prototype.hasOwnProperty.call(item, 'metadata')) {
+                newDevice.metadata[item.id] = [...item.metadata];
+            }
+        });
+
+        return newDevice;
     }
 
     validDevice(device) {

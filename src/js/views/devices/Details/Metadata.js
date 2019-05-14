b40da5607389d72c2ec432351e76a5ae35bd3798
Mariane Previde <mprevide@cpqd.com.br>
2019-05-14 17:14:04 -0300

    meta detail

--- src/js/views/devices/Details/Metadata.js
+++ src/js/views/devices/Details/Metadata.js
@@ -1,35 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import * as i18next from 'i18next';
 
 class Metadata extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            open: true,
+            open: false,
         };
 
         this.showMetadata = this.showMetadata.bind(this);
     }
 
     showMetadata() {
-        this.setState({ open: !this.state.open });
+        this.setState({open: !this.state.open});
     }
 
     render() {
-        const { attr } = this.props;
-        const { open } = this.state;
+        const {attr: {metadata}} = this.props;
+        const {open} = this.state;
         return (
             <div className="metadata-wrapper">
-                {attr.metadata.map(item => (
+                <div className="button" onClick={this.showMetadata}>
+                    {`Meta Attributes (${metadata.length})`}
+                    <i className={`fa ${open ? 'fa-angle-up' : 'fa-angle-down'}`}/>
+                </div>
+                {metadata.map(item => (
                     open && (
-                        <div className="line-metadata">
-                            <div className="label">{item.label}</div>
-                            <div className="value">{item.static_value}</div>
+                        <div className="display-flex-column flex-1">
+                            <div
+                                className="name-value truncate"
+                                title={`${item.label} (${item.type})`}
+                            >
+                                {`${item.label} (${item.type})`}
+                            </div>
+                            <div className="display-flex-no-wrap space-between">
+                                <div
+                                    className="value-value truncate"
+                                    title={item.static_value}
+                                >
+                                    {item.static_value}
+                                </div>
+                                <div
+                                    className="value-label "
+                                    title={item.value_type}
+                                >
+                                    {i18next.exists(`types.${item.value_type}`) ? i18next.t(`types.${item.value_type}`) : item.value_type}
+                                </div>
+
+                            </div>
                         </div>
                     )
                 ))}
-                <div className="button" onClick={this.showMetadata}>
-                    <i className={`fa ${open ? 'fa-angle-up' : 'fa-angle-down'}`} />
-                </div>
+
             </div>
         );
     }

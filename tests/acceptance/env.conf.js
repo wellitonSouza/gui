9707148bd4eb5f092157d69075b3f62a193877f2
Mariane Previde <mprevide@cpqd.com.br>
2019-05-23 10:01:29 -0300

    config

--- tests/acceptance/env.conf.js
+++ tests/acceptance/env.conf.js
@@ -1,6 +1,6 @@
 const env = {
     dojot_host: process.env.DOJOT_HOST || 'http://localhost:8000',
-    mqtt_host: process.env.MQTT_HOST || 'http://10.202.71.108',
+    mqtt_host: process.env.MQTT_HOST || 'http://localhost',
 };
 
 module.exports = env;

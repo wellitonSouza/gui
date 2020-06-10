0381194ceddfde08e9d829819044698179df102e
wellitonSouza <wellitonaz@yahoo.com.br>
2020-06-10 08:56:06 -0300

    change

--- src/js/views/templates/TemplateList/Sidebar/SidebarFirmware/SidebarFirmImages.js
+++ src/js/views/templates/TemplateList/Sidebar/SidebarFirmware/SidebarFirmImages.js
@@ -38,18 +38,18 @@ class SidebarFirmImages extends Component {
 
     onDrop(files, image) {
         const { t } = this.props;
-        if(files && Array.isArray(files) && files.length > 0){
-            if(files[0].name && files[0].name.length > 4){
+        if (files && Array.isArray(files) && files.length > 0) {
+            if (files[0].name && files[0].name.length > 4) {
                 const fileName = files[0].name.substring(
                     files[0].name.length - 4, files[0].name.length,
                 );
-                if(fileName.toUpperCase() !== '.HEX'){                    
+                if (fileName.toUpperCase() !== '.HEX') {
                     toaster.warning(t('firmware:alerts.file_error'));
-                }else{
+                } else {
                     ImageActions.updateImageData(image.id, 'file', files);
                 }
             }
-        }else{    
+        } else {
             toaster.warning(t('firmware:alerts.file_error'));
         }        
     }

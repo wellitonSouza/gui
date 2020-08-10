06f98defb2b59a76bfb2320190096a2cb9e21138
Mariane Previde <mariane.previde@gmail.com>
2020-08-10 13:12:26 -0300

    fix some erros in testes

--- tests/acceptance/env.conf.js
+++ tests/acceptance/env.conf.js
@@ -1,6 +1,8 @@
 const env = {
     dojot_host: process.env.DOJOT_HOST || 'http://localhost:8000',
-    mqtt_host: process.env.MQTT_HOST || 'http://localhost',
+    // dojot_host: process.env.DOJOT_HOST || 'http://10.50.11.181:30001',
+    mqtt_host: process.env.MQTT_HOST || 'tcp://localhost:1883',
+    // mqtt_host: process.env.MQTT_HOST || 'tcp://10.50.11.181:30002',
 };
 
 module.exports = env;

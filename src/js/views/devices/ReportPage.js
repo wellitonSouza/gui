05503688ec9088d1dfd220696539919acbcd2f57
Welliton <wellitonSouza>
2020-10-22 12:12:00 -0300

    report

--- src/js/views/devices/ReportPage.js
+++ src/js/views/devices/ReportPage.js
@@ -16,51 +16,29 @@ class ReportTable extends React.PureComponent {
     componentDidMount() {
         const token = window.localStorage.getItem('jwt');
         const {
-<<<<<<< HEAD
-            deviceId, attrs, dateFrom, dateTo, t, deviceLabel,
-=======
             deviceId, attrs, dateFrom, dateTo, t, deviceLabel, checkClose,
->>>>>>> upstream/development
         } = this.props;
         const URL = `history/device/${deviceId}/history?attr=${attrs.join('&attr=')}&dateFrom=${moment(dateFrom).utc().format('YYYY-MM-DDTHH:mm')}&dateTo=${moment(dateTo).utc().format('YYYY-MM-DDTHH:mm')}`;
         axios.get(URL, { headers: { Authorization: `Bearer ${token}` } }).then((result) => {
             const reportWindow = Array.isArray(result.data) ? (
-<<<<<<< HEAD
-                <NewWindow title={`${deviceLabel} - ${deviceId}`}>
-=======
                 <NewWindow title={`${deviceLabel} - ${deviceId}`} onUnload={checkClose}>
->>>>>>> upstream/development
                     <div className="ReportTitle">{`${deviceLabel} - ${deviceId}`}</div>
                     <Table key="tb-123" itemList={result.data} t={t} />
                 </NewWindow >
             ) : (
-<<<<<<< HEAD
-                    <NewWindow title={`${deviceLabel} - ${deviceId}`}>
+                    <NewWindow title={`${deviceLabel} - ${deviceId}`} onUnload={checkClose}>
                         <div className="ReportTitle">{`${deviceLabel} - ${deviceId}`}</div>
                         {
-=======
-                <NewWindow title={`${deviceLabel} - ${deviceId}`} onUnload={checkClose}>
-                    <div className="ReportTitle">{`${deviceLabel} - ${deviceId}`}</div>
-                    {
->>>>>>> upstream/development
                             Object.keys(result.data).map(
                                 (value) => <Table key="tb-321" itemList={result.data[value]} t={t} />,
                             )
                         }
-<<<<<<< HEAD
                     </NewWindow>
-=======
-                </NewWindow>
->>>>>>> upstream/development
                 );
             this.setState({ reportWindow });
         }).catch(() => {
             const reportWindow = (
-<<<<<<< HEAD
-                <NewWindow title={`${deviceLabel} - ${deviceId}`}>
-=======
                 <NewWindow title={`${deviceLabel} - ${deviceId}`} onUnload={checkClose}>
->>>>>>> upstream/development
                     <div className="ReportTitle">{`${deviceLabel} - ${deviceId}`}</div>
                     <div style={
                         {
@@ -93,10 +71,7 @@ class ReportTable extends React.PureComponent {
 }
 
 ReportTable.propTypes = {
-<<<<<<< HEAD
-=======
     checkClose: PropTypes.func.isRequired,
->>>>>>> upstream/development
     deviceLabel: PropTypes.string.isRequired,
     deviceId: PropTypes.string.isRequired,
     attrs: PropTypes.arrayOf(PropTypes.string).isRequired,

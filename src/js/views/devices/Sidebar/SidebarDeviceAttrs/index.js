ea69f85323fe93e117da4740afe1ae5e1070181a
Welliton <wellitonSouza>
2020-10-22 15:31:38 -0300

    Attribute metadata are not specialized in creating or changing device

--- src/js/views/devices/Sidebar/SidebarDeviceAttrs/index.js
+++ src/js/views/devices/Sidebar/SidebarDeviceAttrs/index.js
@@ -10,66 +10,66 @@ const SidebarDeviceAttrs = ({
     validAttrs,
     handleShowDeviceAttrsDiscard,
     selectAttr,
-    handleChangeMetadata,
+    handleChangeMeta,
     handleChangeAttr,
     deviceAttrsTitle,
     errors,
     t,
 }) => (
-    <Slide right when={showDeviceAttrs} duration={300}>
-        {
-            showDeviceAttrs
-                ? (
-                    <div className="sidebar-device-attrs">
-                        <div className="header">
-                            <div className="title">
-                                {t('devices:manage_attributes')}
+        <Slide right when={showDeviceAttrs} duration={300}>
+            {
+                showDeviceAttrs
+                    ? (
+                        <div className="sidebar-device-attrs">
+                            <div className="header">
+                                <div className="title">
+                                    {t('devices:manage_attributes')}
+                                </div>
+                                <div className="icon">
+                                    <img src="images/icons/chip-cyan.png" alt="device-icon" />
+                                </div>
                             </div>
-                            <div className="icon">
-                                <img src="images/icons/chip-cyan.png" alt="device-icon" />
+                            <div className="body">
+                                <div className="title">
+                                    {`${t('devices:device')} > ${t('text.attribute')}`}
+                                </div>
+                                <div className="attr-type">
+                                    {deviceAttrsTitle}
+                                </div>
+                                <div className="attrs-list">
+                                    {
+                                        selectAttr.map(attr => (
+                                            <AttrCard
+                                                attr={attr}
+                                                key={attr.id}
+                                                handleChangeAttr={handleChangeAttr}
+                                                handleChangeMeta={handleChangeMeta}
+                                                errors={errors[attr.id]}
+                                            />
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            <div className="footer">
+                                <DojotBtnClassic
+                                    onClick={() => handleShowDeviceAttrsDiscard()}
+                                    label={t('discard.label')}
+                                    type="secondary"
+                                />
+                                <DojotBtnClassic
+                                    onClick={() => validAttrs(selectAttr)}
+                                    label={t('save.label')}
+                                    type="primary"
+                                    color="red"
+                                    id="btn-save-attrs"
+                                />
                             </div>
                         </div>
-                        <div className="body">
-                            <div className="title">
-                                {`${t('devices:device')} > ${t('text.attribute')}`}
-                            </div>
-                            <div className="attr-type">
-                                {deviceAttrsTitle}
-                            </div>
-                            <div className="attrs-list">
-                                {
-                                    selectAttr.map(attr => (
-                                        <AttrCard
-                                            attr={attr}
-                                            key={attr.id}
-                                            handleChangeAttr={handleChangeAttr}
-                                            handleChangeMetadata={handleChangeMetadata}
-                                            errors={errors[attr.id]}
-                                        />
-                                    ))
-                                }
-                            </div>
-                        </div>
-                        <div className="footer">
-                            <DojotBtnClassic
-                                onClick={() => handleShowDeviceAttrsDiscard()}
-                                label={t('discard.label')}
-                                type="secondary"
-                            />
-                            <DojotBtnClassic
-                                onClick={() => validAttrs(selectAttr)}
-                                label={t('save.label')}
-                                type="primary"
-                                color="red"
-                                id="btn-save-attrs"
-                            />
-                        </div>
-                    </div>
-                )
-                : <div />
-        }
-    </Slide>
-);
+                    )
+                    : <div />
+            }
+        </Slide>
+    );
 
 SidebarDeviceAttrs.defaultProps = {
     showDeviceAttrs: false,
@@ -80,6 +80,7 @@ SidebarDeviceAttrs.propTypes = {
     validAttrs: PropTypes.func.isRequired,
     t: PropTypes.func.isRequired,
     handleShowDeviceAttrsDiscard: PropTypes.func.isRequired,
+    handleChangeMeta: PropTypes.func.isRequired,
 };
 
 export default withNamespaces()(SidebarDeviceAttrs);

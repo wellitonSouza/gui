28d1ee8ede574d42e54d67511704740e65c8caff
luisfelipefurlan <32846891+luisfelipefurlan@users.noreply.github.com>
2020-10-30 11:48:29 -0300

    fix: Import CSS fix (#690)
    * Import CSS fix
    
    * [CodeFactor] Apply fixes to commit 17e6844
    
    Co-authored-by: Luis Felipe Furlan Cruz <lfcruz@cpqd.com.br>
    Co-authored-by: codefactor-io <support@codefactor.io>

--- src/js/components/importExport/ImportExport.js
+++ src/js/components/importExport/ImportExport.js
@@ -1,54 +1,44 @@
 import PropTypes, { func } from 'prop-types';
 import i18n from 'i18next';
-import React, { Component } from 'react';
+import React from 'react';
 import { DojotBtnClassic } from '../DojotButton';
 
 
-export default class ImportExport extends Component {
-    render() {
-        const {
-            children,
-            handleClick,
-            save,
-            label,
-            closeModal,
-        } = this.props;
-
-        return (
-            <div>
-                <div className="import-export row">
-                    <div className="main-head">
-                        {React.Children.map(children, (child, i) => {
-                        // Aceita só o primeiro elemento filho
-                            if (i >= 1) return null;
-                            return child;
-                        })}
-                    </div>
-                    <div className="main">
-                        {React.Children.map(children, (child, i) => {
-                        // Ignora o primeiro elemento filho
-                            if (i < 1) return null;
-                            return child;
-                        })}
-                    </div>
-                    <div className="footer">
-                        <DojotBtnClassic is_secondary onClick={closeModal} label={i18n.t('cancel.label')} title={i18n.t('cancel.label')} />
-                        {save ? (
-                            <DojotBtnClassic
-                                is_secondary={false}
-                                onClick={handleClick}
-                                label={label}
-                                title={label}
-                            />
-                        )
-                            : null }
-                    </div>
-                </div>
-                <div className="rightsidebar" onClick={closeModal} onKeyPress={closeModal} role="button" tabIndex={0} />
+const ImportExport = ({
+    children, handleClick, save, label, closeModal,
+}) => (
+    <div>
+        <div className="import-export">
+            <div className="main-head">
+                {React.Children.map(children, (child, i) => {
+                    // Aceita só o primeiro elemento filho
+                    if (i >= 1) return null;
+                    return child;
+                })}
             </div>
-        );
-    }
-}
+            <div className="main">
+                {React.Children.map(children, (child, i) => {
+                    // Ignora o primeiro elemento filho
+                    if (i < 1) return null;
+                    return child;
+                })}
+            </div>
+            <div className="footer">
+                <DojotBtnClassic is_secondary onClick={closeModal} label={i18n.t('cancel.label')} title={i18n.t('cancel.label')} />
+                {save ? (
+                    <DojotBtnClassic
+                        is_secondary={false}
+                        onClick={handleClick}
+                        label={label}
+                        title={label}
+                    />
+                )
+                    : null }
+            </div>
+        </div>
+        <div className="rightsidebar" onClick={closeModal} onKeyPress={closeModal} role="button" tabIndex={0} />
+    </div>
+);
 
 ImportExport.propTypes = {
     closeModal: PropTypes.func.isRequired,
@@ -63,3 +53,5 @@ ImportExport.defaultProps = {
     label: '',
     handleClick: func,
 };
+
+export default ImportExport;
